32c26142f9ac8f2b5928ed34dd3d5e9ea2a5549e
TenSE <155313337+TenSEmdf@users.noreply.github.com>
2024-01-11 11:12:12 +0000

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -432,7 +432,7 @@ client.on('interactionCreate', async Interaction => {
 })
 
 
-client.login("prosess.env.token").catch(() => {
+client.login(prosess.env.token).catch(() => {
     console.log(chalk.red('The Token is not valid ❌'))
 })
 process.on('uncaughtException', async () => { return })
